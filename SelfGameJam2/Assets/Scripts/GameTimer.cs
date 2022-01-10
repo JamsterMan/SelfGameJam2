9b4a6bba41f0f33accc92d43d894e472e072109b
JamsterMan <ben.brad.h@gmail.com>
2022-01-10 18:52:56 -0500

    Project made + basic gameplay
    -empty game project
    -Git ignore updated
    -player movement
    -game timer

---
+++ SelfGameJam2/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    //public Text text;
+    public TMP_Text text;
+
+    public int startTimeSec;
+    public int startTimeMin;
+
+    private int minTime;
+    private int secTime;
+    private float timer;
+    private int secTimePass;
+
+    public bool stopTimer;
+
+    private void Start()
+    {
+        stopTimer = false;
+        timer = 0;
+        secTimePass = 0;
+        minTime = 0;
+        secTime = 0;//-1*(startTimeSec % 60);
+        if (startTimeSec % 60 != 0)
+        {
+            startTimeMin += startTimeSec / 60;
+            startTimeSec %= 60;
+        }
+        //formatting timer text
+        if (startTimeSec < 10)
+            text.text = startTimeMin + ":0" + startTimeSec;
+        else
+            text.text = startTimeMin + ":" + startTimeSec;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!stopTimer)
+        {
+            timer += Time.deltaTime;
+            if (Mathf.FloorToInt(timer) > secTimePass)
+            {
+                secTimePass++;
+                secTime++;
+                if (startTimeMin - minTime == 0 && startTimeSec - secTime == 0)//Check if Count down is at 0:00
+                {
+                    Debug.Log("GameWon");
+                    stopTimer = true;
+                }
+                else
+                {
+                    if (startTimeSec - secTime <= 0)
+                    {
+                        secTime = startTimeSec - 59;
+                        minTime++;
+                    }
+                }
+                //formatting timer text
+                if (startTimeSec - secTime < 10)
+                {
+                    text.text = (startTimeMin - minTime) + ":0" + (startTimeSec - secTime);
+                }
+                else
+                {
+                    text.text = (startTimeMin - minTime) + ":" + (startTimeSec - secTime);
+                }
+            }
+        }
+    }
+}
---
+++ SelfGameJam2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    [Range(0.05f,0.3f)]
+    public float smoothMove = 0.2f;
+
+    private Vector2 velocity = Vector2.zero;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float verticalMove = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        rb.velocity = Vector2.SmoothDamp(rb.velocity, new Vector2(horizontalMove, verticalMove), ref velocity, smoothMove);
+    }
+}
